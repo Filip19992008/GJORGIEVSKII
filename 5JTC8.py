7a11039ef17c137d40fc089dc9f7ef256969e4e0
FILIP19992008 <filip2gorgievski@gmail.com>
2024-11-21 19:24:41 +0100

    Kako si

---
+++ 5JTC8.py
@@ -0,0 +1,13 @@
+# sample for @pytest.mark.parametrize
+import pytest
+
+
+def add(a, b):
+    return a + b
+
+
+# Parametrize the test function with different sets of arguments
+@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (0, 0, 0), (-1, -2, -3), (1.5, 2.5, 4.0)])
+def test_add(a, b, expected):
+    result = add(a, b)
+    assert result == expected
